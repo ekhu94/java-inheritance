0435e374dc03928adef8a86c0fd5fa66eb305a43
Erik Huang <ekhu94@gmail.com>
2021-06-29 06:07:52 -0700

    show method downcasting TextBox

--- src/com/ekhu94/Main.java
+++ src/com/ekhu94/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	    var box1 = new TextBox(false);
-	    box1.enable();
-        System.out.println(box1.isEnabled());
+	    show(box1);
+    }
+
+    public static void show(UIControl control) {
+        if (control instanceof TextBox) {
+            var textBox = (TextBox) control;
+            textBox.setText("I am a text box!");
+        }
+        System.out.println(control.toString());
     }
 }
